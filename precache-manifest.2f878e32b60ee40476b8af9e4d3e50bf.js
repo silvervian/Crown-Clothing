818aabbba6ceb31e49f1abd51c5addf04010fafc
Adijos <jedrczakadrian@gmail.com>
2019-12-10 23:26:26 +0100

    Updates

---
+++ precache-manifest.2f878e32b60ee40476b8af9e4d3e50bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c88637cd2ba9952f29a231e55253eab9",
+    "url": "/Crown-Clothing/index.html"
+  },
+  {
+    "revision": "b24179628b696740a3f2",
+    "url": "/Crown-Clothing/static/css/main.dac9987d.chunk.css"
+  },
+  {
+    "revision": "2c137ddce911e506f632",
+    "url": "/Crown-Clothing/static/js/2.a89d7da1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Crown-Clothing/static/js/2.a89d7da1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b24179628b696740a3f2",
+    "url": "/Crown-Clothing/static/js/main.4da2a0e7.chunk.js"
+  },
+  {
+    "revision": "58816446bc59a37178ba",
+    "url": "/Crown-Clothing/static/js/runtime-main.8ea2c933.js"
+  }
+]);
