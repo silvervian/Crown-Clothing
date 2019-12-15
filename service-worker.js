f59dca8f82fc5356e6d3ac667b34e60313d17d0a
Adijos <jedrczakadrian@gmail.com>
2019-12-15 23:15:07 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Crown-Clothing/precache-manifest.6be4175def7103f348076d77a55719f0.js"
+  "/Crown-Clothing/precache-manifest.24b8db4102023cb960cbf5a7ff57705f.js"
 );
 
 self.addEventListener('message', (event) => {
