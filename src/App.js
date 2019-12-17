2622443954bc21cba255095a265cc5efb174a3d7
Adijos <jedrczakadrian@gmail.com>
2019-12-17 22:34:33 +0100

    17.12.2019 - 76

--- src/App.js
+++ src/App.js
@@ -5,13 +5,17 @@ import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import SingInAndSingUp from "./pages/sing-in-and-sing-up/sing-in-and-sing-up.component";
+import Header from "./components/header/header.component";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signIn" component={SingInAndSingUp} />
       </Switch>
     </div>
   );
---
+++ src/pages/sing-in-and-sing-up/sing-in-and-sing-up.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import SingIn from "../../components/sign-in/sign-in.component";
+
+import "./sing-in-and-sing-up.styles.scss";
+
+const SignInAndSignUp = () => (
+  <div className="sing-in-and-sing-up">
+    <SingIn />
+  </div>
+);
+
+export default SignInAndSignUp;
