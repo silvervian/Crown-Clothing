b88298e078c8e0576b7e16887b380f05b5b3de9d
Adijos <jedrczakadrian@gmail.com>
2020-01-11 22:44:09 +0100

    some changes

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import App from "./App";
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter basename="/Crown-Clothing">
+    <BrowserRouter>
       <PersistGate persistor={persistor}>
         <App />
       </PersistGate>
--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
+import StripeButton from "../../components/stripe-button/stripe-button.component";
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
 
 import {
@@ -36,6 +37,12 @@ const CheckoutPage = ({ cartItems, total }) => (
     <div className="total">
       <span>TOTAL: ${total}</span>
     </div>
+    <div className="test-warning">
+      *Please use the following test credit card for payments*
+      <br />
+      4242 4242 4242 42 42 - Exp: 01/20 - CVV: 123
+    </div>
+    <StripeButton price={total} />
   </div>
 );
 
