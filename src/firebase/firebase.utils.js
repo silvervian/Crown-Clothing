2103f66f5f1b623f1bab35eafebe8600d31002ff
Adrian Jedrczak <adrianjedrczak@gmail.com>
2020-01-16 11:56:38 +0100

    16.01.2020 - 163

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -54,6 +54,25 @@ export const addCollectionAndDocuments = async (
 
   return await batch.commit();
 };
+
+export const convertCollectionsSnapshotToMap = collections => {
+  const transformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    };
+  });
+
+  transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
+};
+
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
